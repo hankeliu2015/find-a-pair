6b32e98c1f835670a23d2272c395670f873b4b3c
Hank Liu <hanke.liu@gmail.com>
2020-05-01 17:21:16 -0400

    create pair model and establish association with user model

---
+++ app/models/pair.rb
@@ -0,0 +1,5 @@
+class Pair < ApplicationRecord
+  belongs_to :requestor_user, class_name: "User"
+  belongs_to :respondor_user, class_name: "User"
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_01_004433) do
+ActiveRecord::Schema.define(version: 2020_05_01_210015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pairs", force: :cascade do |t|
+    t.string "topic"
+    t.integer "requestor_user_id"
+    t.integer "respondor_user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
