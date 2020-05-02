2009d463638f819f4f67fa8b04841fa23da947a7
Hank Liu <hanke.liu@gmail.com>
2020-05-01 23:19:01 -0400

    generate pairs controller and pairs routes

---
+++ app/controllers/pairs_controller.rb
@@ -0,0 +1,7 @@
+class PairsController < ApplicationController
+  def new
+  end
+
+  def create
+  end 
+end
--- app/models/pair.rb
+++ app/models/pair.rb
@@ -1,5 +1,5 @@
 class Pair < ApplicationRecord
   belongs_to :requestor_user, class_name: "User"
-  belongs_to :respondor_user, class_name: "User"
+  belongs_to :respondor_user, class_name: "User", optional: true
 
 end
