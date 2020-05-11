3cd03e5562058e3f20bdd2df1c64f7cd8f70766e
Hank Liu <hanke.liu@gmail.com>
2020-05-11 17:51:51 -0400

    nav bar hamberg button still not working

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require("webpack");
 
-module.exports = environment
+environment.plugins.prepend('provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
+  })
+)
+
+module.exports = environment;
